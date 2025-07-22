50df46323520af8d5f8ca74c114f7d2e7f30a613
Ali nadhum <alinadhum1998@gmail.com>
2025-07-22 18:47:33 +0300

    add register controller

--- Controllers/Auth/AuthController.js
+++ Controllers/Auth/AuthController.js
@@ -1,12 +1,9 @@
 const { PrismaClient } = require("@prisma/client");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-
-
+const { HashPassword } = require("../../Utils/HashPassword");
 
 const prisma = new PrismaClient();
-
-
 
 // ==================================
 // @desc Register new user
@@ -14,3 +11,36 @@ const prisma = new PrismaClient();
 // @method POST
 // @access public
 // ==================================
+module.exports.register = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "This user already exists" });
+  }
+
+  const hashedPassword = await HashPassword(password);
+
+  let imagePath;
+  if (req.file) {
+    imagePath = `/assets/${req.file.filename}`;
+  } else {
+    imagePath = "/assets/avatar.jpg";  
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      image: imagePath,
+      role,
+      block: false,
+    },
+  });
+
+  res.status(201).json({ message: "User registered successfully" });
+});
