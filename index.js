5a63f69a24a2589036b193a2e2490d9ebcab3cba
Ali nadhum <alinadhum1998@gmail.com>
2025-07-22 18:22:19 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express  = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const path = require('path');
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+
+
+// Run server
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log(`Server is run on port ${port}`));
