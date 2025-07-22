4d6f32e51b2d7c6068133b6e609f6447170e1fca
Ali nadhum <alinadhum1998@gmail.com>
2025-07-22 18:32:51 +0300

    add multer config

---
+++ middlewares/uploadImage.js
@@ -0,0 +1,29 @@
+
+
+// ========= Old code ========
+
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split("/")[1];
+    const fileName = `${Date.now()}.${ext}`;
+    cb(null, fileName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
+const uploadImage = multer({ storage, fileFilter });
+
+module.exports = uploadImage;
